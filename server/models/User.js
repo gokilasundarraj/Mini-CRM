0503356e4b57941dd1c0fecb76b9872b29dad67b
gokilasundarraj <gokilas333@gmail.com>
2026-02-22 15:32:55 +0530

    server config fixed

--- server/models/User.js
+++ server/models/User.js
@@ -22,13 +22,11 @@ const userSchema = new mongoose.Schema({
     }
 }, { timestamps: true });
 
-// Hash password before saving
 userSchema.pre('save', async function () {
     if (!this.isModified('password')) return;
     this.password = await bcrypt.hash(this.password, 10);
 });
 
-// Compare password method
 userSchema.methods.comparePassword = async function (candidatePassword) {
     return await bcrypt.compare(candidatePassword, this.password);
 };
